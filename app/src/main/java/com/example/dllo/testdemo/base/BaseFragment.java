06ad8842db89e87c45a04cb2b4143302ac744b5c
任杰 <ren_ddang@163.com>
2016-10-21 21:00:16 +0800

    基类

--- app/src/main/java/com/example/dllo/testdemo/base/BaseFragment.java
+++ app/src/main/java/com/example/dllo/testdemo/base/BaseFragment.java
@@ -1,5 +1,6 @@
 package com.example.dllo.testdemo.base;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,14 @@ import com.example.dllo.testdemo.R;
  * Fragment的基类
  */
 public abstract class BaseFragment extends Fragment {
+    protected Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,6 +46,12 @@ public abstract class BaseFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initData();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // 销毁广播
     }
 
     protected <T extends View> T bindView(int id) {
